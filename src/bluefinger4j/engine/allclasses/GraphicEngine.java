2c628381f6dbfc0e49196e5ef164fd69ef03c649
Renan <renan90@outlook.com>
2017-12-31 20:27:08 -0200

    Adicionado GraphicEngine

---
+++ src/bluefinger4j/engine/allclasses/GraphicEngine.java
@@ -0,0 +1,94 @@
+/*
+ *Copyright (c) 2014, Renan T. dos Santos
+ *All rights reserved.
+ *
+ *Redistribution and use in source and binary forms, with or without
+ *modification, are permitted provided that the following conditions are met:
+ *
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package bluefinger4j.engine.allclasses;
+
+import bluefinger4j.engine.allinterfaces.IGraphicEngine;
+import bluefinger4j.graphics.node.allclasses.GraphicNode;
+import bluefinger4j.pixel.allinterfaces.BitmapAdapter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Motor grafico, serve de meio de comunicaçao entre as classes do Bluefinger4j
+ * e o componentes graficos que vão renderizar as imagens. 
+ * 
+ * @author Renan
+ */
+public class GraphicEngine implements IGraphicEngine {
+
+       
+    private final GraphicNode root;
+    private Map<String,BitmapAdapter> textures;
+    
+    /**
+     * Cria o GraphicEngine com o node que sera a raiz dos nodes processados
+     * @param root node raiz a ser processado nessa instância
+     */
+    public GraphicEngine(GraphicNode root) {
+        this.root = root;
+    }
+    
+    /**
+     * Registra uma textura
+     * @param name nome da textura
+     * @param texture o BitmapAdapter que servira da textura
+     * @return retorna this
+     */
+    @Override
+    public GraphicEngine register(String name , BitmapAdapter texture){
+        if(this.textures == null) this.textures = new HashMap<String, BitmapAdapter>() ;
+        this.textures.put(name, texture);
+        return this;
+    }
+    
+    /**
+     * Deregistra uma textura
+     * @param name nome da textura a ser removida 
+     * @return retorna this
+     */
+    @Override
+    public GraphicEngine unregister(String name){
+        if(this.textures != null) this.textures.remove(name);
+        return this;
+    }
+    
+    /**
+     * Dispara o driver para o mesmo processar os nodes
+     * @param driver Objeto que serve de driver, ou seja, faz algo como a saida do Bluefinger4j
+     * @return retorna this.
+     */
+    @Override
+    public GraphicEngine compile(IGraphicEngine.GraphicDriver driver){
+        for(int i = 0 ; i < this.root.getNodeCount(); i++){
+           driver.encode(this, this.root.getNode(i), textures);
+        }
+         driver.encode(this, root, textures);
+        return this;
+    }
+    
+}
---
+++ src/bluefinger4j/engine/allinterfaces/IGraphicEngine.java
@@ -0,0 +1,78 @@
+/*
+ *Copyright (c) 2014, Renan T. dos Santos
+ *All rights reserved.
+ *
+ *Redistribution and use in source and binary forms, with or without
+ *modification, are permitted provided that the following conditions are met:
+ *
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package bluefinger4j.engine.allinterfaces;
+
+import bluefinger4j.engine.allclasses.GraphicEngine;
+import bluefinger4j.graphics.node.allinterfaces.IGraphicNode;
+import bluefinger4j.pixel.allinterfaces.BitmapAdapter;
+import java.util.Map;
+
+/**
+ *
+ * @author Renan
+ */
+public interface IGraphicEngine {
+    
+    /**
+    * Driver grafico da Bluefinger4j essa é a ponte entre as classes do Bluefinger4j
+    * e os componentes externos.
+    */ 
+    public interface GraphicDriver {
+        /**
+         * Codifica os dados que estão nos nodes do Bluefinger4J para os componentes externos.
+         * @param context o GraphicEngine que esse driver esta associado
+         * @param node node que será tratado pelo driver
+         * @param textures hash contendo o nome da textura como chave e a textura como valor. Nao é obrigatorio o uso de texturas.
+         */
+        public void encode(GraphicEngine context , IGraphicNode node , Map<String,BitmapAdapter> textures );
+    }
+
+
+    /**
+     * Dispara o driver para o mesmo processar os nodes
+     * @param driver Objeto que serve de driver, ou seja, faz algo como a saida do Bluefinger4j
+     * @return retorna this.
+     */
+    GraphicEngine compile(GraphicDriver driver);
+
+    /**
+     * Registra uma textura
+     * @param name nome da textura
+     * @param texture o BitmapAdapter que servira da textura
+     * @return retorna this
+     */
+    GraphicEngine register(String name, BitmapAdapter texture);
+
+    /**
+     * Deregistra uma textura
+     * @param name nome da textura a ser removida
+     * @return retorna this
+     */
+    GraphicEngine unregister(String name);
+    
+}
